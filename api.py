d9fea17f48efec76d47dccad66a16e7a930f34c4
ariffo <andres.riffo@rankmi.com>
2021-07-21 18:09:24 -0400

    Endpoints listos y funcionando

--- api.py
+++ api.py
@@ -1,16 +1,56 @@
 from flask import Flask, jsonify, request
-import yfinance as yf
+from stock_info import stock_info
 
 app = Flask(__name__)
-
-start = '2000-01-01'
-end = '2000-01-05'
 
 
 @app.route('/<string:name>')
 def all_info(name):
     data_request = request.get_json()
-    return jsonify(yf.download('AAPL', start=start, end=end, progress=False).to_dict())
+    dict_info = stock_info(name, data_request['start'], data_request['end'])
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/open')
+def open_price_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='open')
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/close')
+def close_price_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='close')
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/high')
+def high_price_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='high')
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/low')
+def low_price_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='low')
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/adj-close')
+def adj_close_price_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='adj close')
+    return jsonify(dict_info)
+
+
+@app.route('/<string:name>/volume')
+def volume_stock_info(name):
+    data_request = request.get_json()
+    dict_info = stock_info(name, data_request['start'], data_request['end'], only='volume')
+    return jsonify(dict_info)
 
 
 app.run(port=6787)
--- stock_info.py
+++ stock_info.py
@@ -1,18 +1,27 @@
 import yfinance as yf
+from enum import Enum
+
+
+class SpecificInfo(Enum):
+    OPEN = 'Open'
+    CLOSE = 'Close'
+    LOW = 'Low'
+    HIGH = 'High'
+    ADJ_CLOSE = 'Adj Close'
+    VOLUME = 'Volume'
 
 
 def stock_info(ticker, start, end, only=None):
     if only is None:
         df_yahoo = yf.download(ticker, start=start, end=end, progress=False).to_dict()
-        dict_stock_info = {stock_info: {str(date.to_pydatetime().date()): value for date, value in dict_info.items()}
-                           for stock_info, dict_info in df_yahoo.items()}
+        dict_stock_info = {info_stock: {str(date.to_pydatetime().date()): value for date, value in dict_info.items()}
+                           for info_stock, dict_info in df_yahoo.items()}
         return dict_stock_info
-    else:
+    elif only.title() in [x.value for x in SpecificInfo]:
         df_yahoo = yf.download(ticker, start=start, end=end, progress=False).to_dict()
         dict_stock_info = {info_stock: {str(date.to_pydatetime().date()): value for date, value in dict_info.items()}
                            for info_stock, dict_info in df_yahoo.items()}
-        return {only: dict_stock_info[only]}
-
-
-print(stock_info('AAPL', start='2000-01-01', end='2000-01-05', only='Open'))
-
+        return {only.title(): dict_stock_info[only.title()]}
+    else:
+        return {"message": "Specific information key is invalid. Only 'Open', 'Close', 'High', "
+                           "'Low', 'Adj Close' and 'Volume' are accepted!"}
