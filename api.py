787a3564bba2dd12d0dbe632b2466d2de65488ff
ariffo <andres.riffo@rankmi.com>
2021-07-23 17:33:49 -0400

    initial

--- api.py
+++ api.py
@@ -1,62 +1,75 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_restful import Resource, Api
 from stock_info import stock_info
 from today import today
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/<string:name>')
-def all_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'])
-    return jsonify(dict_info)
+class StockAllInfo(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'])
+        return dict_info, 200 if dict_info['Open'] else 404
 
 
-@app.route('/<string:name>/open')
-def open_price_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='open')
-    return jsonify(dict_info)
+class OpenPriceShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='open')
+        return dict_info, 200 if dict_info['Open'] else 404
 
 
-@app.route('/<string:name>/close')
-def close_price_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='close')
-    return jsonify(dict_info)
+class ClosePriceShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='close')
+        return dict_info, 200 if dict_info['Close'] else 404
 
 
-@app.route('/<string:name>/high')
-def high_price_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='high')
-    return jsonify(dict_info)
+class LowPriceShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='low')
+        return dict_info, 200 if dict_info['Low'] else 404
 
 
-@app.route('/<string:name>/low')
-def low_price_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='low')
-    return jsonify(dict_info)
+class HighPriceShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='high')
+        return dict_info, 200 if dict_info['High'] else 404
 
 
-@app.route('/<string:name>/adj-close')
-def adj_close_price_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='adj close')
-    return jsonify(dict_info)
+class AdjClosePriceShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='adj close')
+        return dict_info, 200 if dict_info['Adj Close'] else 404
 
 
-@app.route('/<string:name>/volume')
-def volume_stock_info(name):
-    data_request = request.get_json()
-    dict_info = stock_info(name, data_request['start'], data_request['end'], only='volume')
-    return jsonify(dict_info)
+class VolumeShare(Resource):
+    def get(self, name):
+        data_request = request.get_json()
+        dict_info = stock_info(name, data_request['start'], data_request['end'], only='volume')
+        return dict_info, 200 if dict_info['Volume'] else 404
 
 
-@app.route('/<string:name>/today')
-def today_price(name):
-    return jsonify(today(name))
+api.add_resource(StockAllInfo, '/<string:name>')
+api.add_resource(OpenPriceShare, '/<string:name>/open')
+api.add_resource(OpenPriceShare, '/<string:name>/close')
+api.add_resource(OpenPriceShare, '/<string:name>/low')
+api.add_resource(OpenPriceShare, '/<string:name>/high')
+api.add_resource(OpenPriceShare, '/<string:name>/adj-close')
+api.add_resource(OpenPriceShare, '/<string:name>/volume')
 
 
 app.run(port=6787)
+
+# @app.route('/<string:name>/today')
+# def today_price(name):
+#     return jsonify(today(name))
+
+
+
